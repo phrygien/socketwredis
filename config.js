64ac538e40363dc002e60919eb38ea30bb1d4b49
mecene phrygien <mecenephrygien@gmail.com>
2026-06-18 09:28:50 +0400

    Port

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // ─── Configuration ────────────────────────────────────────────────────────────
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3006;
 
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['*'];
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : ["*"];
 
 module.exports = { PORT, ALLOWED_ORIGINS };
